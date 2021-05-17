577f6509ec2c12b7f6094ca0f6ee2d839a6fa912
Kristian MacDonald <kristian.macsempreezy@gmail.com>
2021-05-17 10:39:37 -0700

    lots o' changes: big push

--- app.js
+++ app.js
@@ -3,47 +3,106 @@ const grid = document.querySelector('.grid-container');
 const gridDiv = document.querySelectorAll('.grid-div');
 const resetBtn = document.getElementById('reset');
 const slider = document.getElementById("myRange");
-const output = document.getElementById("demo");
+const output = document.querySelector('.output');
+output.textContent = slider.value;
 
-output.innerHTML = slider.value;
 
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
 //Global variables
-const defaultGrid = 16; //Max size 64.
-let input;
-
+let div;
 
 //Functions
 const setDefaultGrid = () => {
+  const defaultGrid = 16; //Max size 64.
   setGridSize(defaultGrid);
 };
 
-const setGridSize = num => {
-  const gridSize = grid.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+const setGridSize = num => { 
+  clearGrid()
+  grid.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
   createBlocks(num);
 }
 
-const randomColor = (e) => {
+const addColor = (e) => {
   const random = () => Math.floor(Math.random() * 255);
   const color = `rgb(${random()}, ${random()}, ${random()})`;
+  if (!e.target.style.backgroundColor) { 
   e.target.style.backgroundColor = color;
+  } else {
+    rgb = e.target.style.backgroundColor;
+    e.target.style.backgroundColor = darkenByTenth(rgb);
+  }
+};
+
+function getLowestMiddleHighest(rgbIntArray) {
+  let highest = {val:-1,index:-1};
+  let lowest = {val:Infinity,index:-1};
+
+  rgbIntArray.map((val,index)=>{
+    if(val>highest.val){
+      highest = {val:val,index:index};
+    }
+    if(val<lowest.val){
+      lowest = {val:val,index:index};
+    }
+  });
+
+  if(lowest.index===highest.index){
+    lowest.index=highest.index+1;
+  }
+  
+  let middle = {index: (3 - highest.index - lowest.index)};
+  middle.val = rgbIntArray[middle.index];
+  return [lowest,middle,highest];
 }
 
-const createBlocks = gridSize => {
-  for (let i=0; i < (gridSize * gridSize); i++) {
-    const div = document.createElement('div');
-    div.addEventListener('mouseover', randomColor);
+function darkenByTenth(rgb) {
+  
+  const rgbIntArray = rgb.replace(/ /g, '').slice(4, -1).split(',').map(e => parseInt(e));
+  const [lowest,middle,highest] = getLowestMiddleHighest(rgbIntArray);
+  
+  if(highest.val===0){
+    return rgb;
+  }
+
+  const returnArray = [];
+
+  returnArray[highest.index] = highest.val-(Math.min(highest.val,25.5));
+  const decreaseFraction  =(highest.val-returnArray[highest.index])/ (highest.val);
+  returnArray[middle.index]= middle.val -middle.val*decreaseFraction; 
+  returnArray[lowest.index]= lowest.val -lowest.val*decreaseFraction;              
+                            
+  return (`rgb(${returnArray.join()}) `);
+}
+
+const createBlocks = num => {
+  for (let i=0; i < (num * num); i++) {
+    div = document.createElement('div');
+    div.addEventListener('mouseover', addColor);
     grid.appendChild(div);
   };
 }
 
-const resetGridColor = () => {
-  console.log(gridDiv.length);
-  // gridDiv.map(div => div.style.backgroundColor = rgb(255, 252, 252));
+const clearGrid = () => {
+  const gridElements = [...grid.childNodes];
+  gridElements.forEach(element => element.remove());
 };
 
+const resetGridColor = (e) => {
+  const gridElements = [...grid.childNodes];
+  gridElements.forEach(element => element.style.backgroundColor = 'rgb(255, 252, 252)');
+};
+
+
 //Event Listeners
+
+slider.addEventListener('input', function() {
+  setGridSize(this.value);
+  output.textContent = this.value;
+  return output;
+});
+
 resetBtn.addEventListener('click', resetGridColor);
+resetBtn.addEventListener('click', function() {
+  setGridSize(output.textContent);
+});
 window.addEventListener('load', setDefaultGrid);
